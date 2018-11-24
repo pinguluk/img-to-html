c1e4c59948b84d23c5812d366619e4eeed2ffe19
pinguluk <luca_paun@yahoo.com>
2018-11-24 04:18:06 +0200

    now it checks for transparent pixel & added comments

--- img.py
+++ img.py
@@ -1,7 +1,6 @@
 from PIL import Image
-import cv2
-import numpy
 
+# function rgb => hex
 def rgb2hex(rgb):
     r = rgb[0]
     g = rgb[1]
@@ -9,31 +8,42 @@ def rgb2hex(rgb):
     hex = "#{:02x}{:02x}{:02x}".format(r,g,b)
     return hex
 
+# image name + extension
+filename = "2018.png"
+# load image + convert to RGBA
+image = Image.open(filename).convert('RGBA')
+# load pixels
+pixeldata = image.load()
 
-image = cv2.imread("logo.png")
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-#contor
-i = 0
-# grab the image dimensions
-h = image.shape[0]
-w = image.shape[1]
-
+# open / create html file
 file = open('index.html', 'w')
+# write basic stuff
 file.write('<!DOCTYPE html><html><head><style>#image{display:flex;flex-direction:column}.row div{display:inline-block;float:left;width:1px;height:1px;}</style></head><body><div id="image">')
 
-# loop over the image, pixel by pixel
-for y in range(0, h):
+#image height & width
+width, height = image.size
+
+# loop rows
+for y in range(height):
+    # create row div
     file.write('<div class="row">')
-    for x in range(0, w):
-        hexValue = rgb2hex(image[y, x])
-        #print(hexValue)
-        file.write('<div style="background:' + hexValue + '"></div>')
-        i = i + 1
-    file.write('</div></body></html>')
+    # loop columns
+    for x in range(width):
+        # print current pixel
+        # print(pixeldata[x, y])
+        # if current pixel is transparent => write empty div
+        if pixeldata[x, y][3] == 0:
+            file.write('<div></div>')
+        # else write the current pixel to hex value
+        else:
+            hexValue = rgb2hex(pixeldata[x,y])
+            file.write('<div style="background:' + hexValue + '"></div>')
+    # close row div
+    file.write('</div>')
 
-
-file.write('<div></div>')
+# close "image" div
+file.write('</div>')
+# close body & html tags
 file.write('</body></html>')
 
 
